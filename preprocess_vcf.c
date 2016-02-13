c723eba4ee91928c81b3676092472150a10c9aef
maiziex <maiziex@gmail.com>
2016-02-12 22:46:37 -0800

    second draft

--- preprocess_vcf.c
+++ preprocess_vcf.c
@@ -1,11 +1,18 @@
+#include "string.h"
+#include "stdlib.h"
+#include "limits.h"
+#include "math.h"
 #include "common.h"
 
 typedef struct
 {
     int64_t candidate_num;
     int64_t max_candidate_num;
-    int64_t* candiate_loci;
+    int64_t* candidate_loci;
+    int* flag;
     int** GT;
+    float* AF;
+    
 } block_t;
 
 typedef struct
@@ -14,26 +21,62 @@ typedef struct
     int sample_name_max_length;
 } pars_t;
 
+typedef struct
+{
+    int64_t candidate_num;
+    int64_t max_candidate_num;
+    int64_t* candidate_loci;
+    float* knn;
+} indel_knn_t;
 
 
-block_t* init_block(int64_t max_candidate_num)
+
+block_t* init_block(int64_t max_candidate_num, block_t* block, pars_t* pars)
 {
     block->candidate_num=0;
     block->max_candidate_num = max_candidate_num;
     block->candidate_loci = (int64_t*) malloc(block->max_candidate_num*sizeof(int64_t));
-    block->GT = (int**) malloc(block->max_candidate_num*sizeof(int*));\
+    block->flag = (int*) malloc(block->max_candidate_num*sizeof(int));
+    block->AF = (float*) malloc(block->max_candidate_num*sizeof(float));
+    block->GT = (int**) malloc(block->max_candidate_num*sizeof(int*));
+    int i;
     for(i=0;i<block->max_candidate_num;i++)
     {
         block->GT[i]=(int*) malloc(pars->sample_num*sizeof(int));
     }
- 
+    return block;
 }
+
+indel_knn_t* init_indel_knn(int64_t max_candidate_num,indel_knn_t* indel_knn)
+{
+    indel_knn->candidate_num = 0;
+    indel_knn->max_candidate_num = max_candidate_num;
+    indel_knn->candidate_loci= (int64_t*) malloc(indel_knn->max_candidate_num*sizeof(int64_t));
+    indel_knn->knn= (float*) malloc(indel_knn->max_candidate_num*sizeof(float));
+
+    return indel_knn;
+}
+
+void check_recalloc_indel_knn(indel_knn_t* indel_knn)
+{
+    if(indel_knn->candidate_num>= indel_knn->max_candidate_num)
+    {
+        indel_knn->candidate_loci = (int64_t*) realloc(indel_knn->candidate_loci, indel_knn->max_candidate_num*2*sizeof(int64_t));
+        indel_knn->knn = (float*) realloc(indel_knn->knn, indel_knn->max_candidate_num*2*sizeof(float));
+        indel_knn->max_candidate_num *=2;
+    }
+}
+
+
+
 
 void recalloc_gl_block(block_t* block, pars_t* pars)
 {
-	block->candidate_loci = (int64_t*) realloc(block->candidate_loci, block->max_candidate_num * 2 * sizeof(int64_t));
-
-	int64_t i, j;
+	block->candidate_loci = (int64_t*) realloc(block->candidate_loci, block->max_candidate_num*2*sizeof(int64_t));
+	block->flag = (int*) realloc(block->flag, block->max_candidate_num*2*sizeof(int));
+    block->AF = (float *) realloc(block->AF, block->max_candidate_num*2*sizeof(float));
+    block->GT = (int**) realloc(block->GT, block->max_candidate_num*2*sizeof(int*));
+	int64_t i;
 	for(i = block->max_candidate_num; i < 2 * block->max_candidate_num; i++)
     {
             block->GT[i] = (int*) malloc(pars->sample_num * sizeof(int));
@@ -43,71 +86,65 @@ void recalloc_gl_block(block_t* block, pars_t* pars)
 	//printf("block size increase to %d\n", block->max_candidate_num);
 }
 
-void add_candidate_into_gl_block(block_t* block, pars_t* pars, int64_t locus,int** saved_GT)
+void add_candidate_into_gl_block(block_t* block, pars_t* pars, int64_t locus)
 {
 	block->candidate_loci[block->candidate_num] = locus;
-	int i;
-	/*for(i = 0; i < pars->sample_num; i++)
-	{
-        block->GT[block->candidate_num][i] = saved_GT[block->candidate_num][i];
-	}*/
-
 	block->candidate_num++;
-	if(block->candidate_num >= block->max_candidate_num)
-	{
-		recalloc_gl_block(block, pars);
-	}
-}
-
-
-
-void init_query_samples(char* input_filename, pars_t* pars)
-{
-	FILE* input = fileOpenR(input_filename);
-
-	int field_index = 0, sample_name_length;
-        pars->sample_name_max_length = 0;
-        char c, c_prev;
-
-	while((c = (char) getc(input)) != EOF)
+        if(block->candidate_num >= block->max_candidate_num)
         {
-                if(c == '#')
-                {
-                        c = (char) getc(input);
-                        if(c == '#')
-                        {
-                                while(c != '\n') c = (char) getc(input);
-                        }
-                        else
-                        {
-                                while(c != '\n')
-                                {
-                                        c_prev = c;
-                                        c = (char) getc(input);
-                                        if(c == '\n') break;
-                                        if((c_prev == ' ' || c_prev == '\t') && (c != ' ' && c != '\t'))
-                                        {
-                                                field_index++;
-                                                if(field_index > VCF_MANDATORY)
-                                                {
-                                                        sample_name_length = 1;
-                                                }
-                                        }
-                                        else if((c_prev != ' ' || c_prev != '\t') && (c != ' ' && c != '\t'))
-                                        {
-                                                if(field_index > VCF_MANDATORY)
-                                                {
-                                                        sample_name_length++;
-                                                }
-                                        }
-                                        else
-                                        {
-                                                if(field_index > VCF_MANDATORY && sample_name_length > pars->sample_name_max_length)
-                                                {
-                                                        pars->sample_name_max_length = sample_name_length;
-                                                }
-                                        }
-                                }
+            recalloc_gl_block(block, pars);
+        }
+ }
+
+
+
+    void init_query_samples(char* input_filename, pars_t* pars)
+    {
+        FILE* input = fileOpenR(input_filename);
+
+        int field_index = 0, sample_name_length;
+            pars->sample_name_max_length = 0;
+            char c, c_prev;
+
+        while((c = (char) getc(input)) != EOF)
+            {
+                    if(c == '#')
+                    {
+                            c = (char) getc(input);
+                            if(c == '#')
+                            {
+                                    while(c != '\n') c = (char) getc(input);
+                            }
+                            else
+                            {
+                                    while(c != '\n')
+                                    {
+                                            c_prev = c;
+                                            c = (char) getc(input);
+                                            if(c == '\n') break;
+                                            if((c_prev == ' ' || c_prev == '\t') && (c != ' ' && c != '\t'))
+                                            {
+                                                    field_index++;
+                                                    if(field_index > VCF_MANDATORY)
+                                                    {
+                                                            sample_name_length = 1;
+                                                    }
+                                            }
+                                            else if((c_prev != ' ' || c_prev != '\t') && (c != ' ' && c != '\t'))
+                                            {
+                                                    if(field_index > VCF_MANDATORY)
+                                                    {
+                                                            sample_name_length++;
+                                                    }
+                                            }
+                                            else
+                                            {
+                                                    if(field_index > VCF_MANDATORY && sample_name_length > pars->sample_name_max_length)
+                                                    {
+                                                            pars->sample_name_max_length = sample_name_length;
+                                                    }
+                                            }
+                                    }
 				break;
                         }
                 }
@@ -117,16 +154,80 @@ void init_query_samples(char* input_filename, pars_t* pars)
 	fileClose(input);
 }
 
+int cmpfunc(const void* a, const void* b)
+{
+    return (*(float*)a - *(float*)b);
+}
+
+
+void find_KNN(block_t* block, pars_t* pars, indel_knn_t* indel_knn) 
+{
+
+  int i,j,k,m,l;
+  int union_counter = 0;
+  int intersection_counter = 0;
+  float SI,knnSI;
+  float* saved_SI = calloc(block->candidate_num,sizeof(float));
+  for(i = 0; i < block->candidate_num; i++)
+  {
+      if(block->flag[i]==1)  //indel locus
+      {
+           m = 0;
+           for(j=0; j< block->candidate_num;j++)
+           {
+               if(block->flag[j]==0)  // snp locus
+               {
+                     saved_SI[m] = 0;
+                     for(k=0; k< pars->sample_num;k++)
+                     {
+                         if ((block->GT[i][k]==1 || block->GT[i][k]==2) ||  (block->GT[j][k]==1 || block->GT[j][k]==2))
+                         {
+                             union_counter ++;
+                         }
+                         
+                         if ((block->GT[i][k]==1 || block->GT[i][k]==2) &&  (block->GT[j][k]==1 || block->GT[j][k]==2))
+                         {
+                             intersection_counter ++;
+                         }
+                     }
+                     SI =(float) intersection_counter*(1.0)/union_counter;
+                     printf("%f",SI);
+                     saved_SI[m++]=SI;
+               }
+
+           }
+
+           qsort(saved_SI,block->candidate_num,sizeof(float),cmpfunc);
+           for(l=block->candidate_num-4; l< block->candidate_num;l++)
+           {
+               knnSI =  knnSI + saved_SI[l];
+           }
+
+           indel_knn->candidate_loci[indel_knn->candidate_num] = i;
+           indel_knn->knn[indel_knn->candidate_num] = knnSI/4.0;
+           indel_knn->candidate_num++;  
+           check_recalloc_indel_knn(indel_knn);
+      }
+
+  }
+
+
+}
 
 
 
 
-void read_vcf_data(CHAR *input_filename,pars_t* pars)
+
+void read_vcf_data(char* input_filename, pars_t* pars)
 {
     init_query_samples(input_filename,pars);
-    char c;
-    block_t* block;
-    init_block(100);
+    char c,c_prev,c_next;
+    block_t* block =  (block_t*) calloc(1,sizeof(block_t));
+    indel_knn_t* indel_knn =  (indel_knn_t*) calloc(1,sizeof(indel_knn_t));
+    init_block(100,block,pars);
+    init_indel_knn(100,indel_knn);
+
+
     FILE* input = fileOpenR(input_filename);
     while( (c= (char) getc(input)) != EOF)
     {    if(c == '#')
@@ -145,22 +246,14 @@ void read_vcf_data(CHAR *input_filename,pars_t* pars)
     }
 
     int i, j;
-	int field_length; 
+	int field_length,ref_length,alt_length; 
 	int64_t locus;
-    int max_candidate_num = 100;
-    int num_of_semicolon_before_AF,numof_colon_before_GT,freeze; 
+    float af;
+    int num_of_semicolon_before_AF,num_of_colon_before_GT,freeze; 
     char* chr = calloc(MAX_CHROMOSOME_LENGTH, sizeof(char));
 	char* ref = calloc(MAX_CHROMOSOME_LENGTH, sizeof(char));
     char* alt = calloc(MAX_CHROMOSOME_LENGTH, sizeof(char));
-    float* AF = calloc(MAX_LOCUS_LENGTH,sizeof(float));
-    /*saved_GT = (int**) malloc(block->mm*sizeof(int*));
-    for(i=0;i<block->max_candidate_num;i++)
-    {
-        saved_GT[i]=(int*) malloc(pars->sample_num*sizeof(int));
-    }*/
- 
-
-
+    
     c = (char) getc(input);
 
 	while(c != EOF)
@@ -190,6 +283,8 @@ void read_vcf_data(CHAR *input_filename,pars_t* pars)
 			ref[field_length] = c;
 			field_length++;
 		}
+        ref_length = field_length;
+        
 
 		memset(alt, 0, MAX_CHROMOSOME_LENGTH * sizeof(char));
 		field_length = 0;
@@ -198,6 +293,17 @@ void read_vcf_data(CHAR *input_filename,pars_t* pars)
             alt[field_length] = c;
             field_length++;
         }
+        alt_length = field_length;
+
+        if(ref_length == 1 && alt_length ==1)
+        {
+            block->flag[block->candidate_num] = 0; //snp
+        }
+        else
+        {
+            block->flag[block->candidate_num] = 1; //indel
+        }
+
 
 		for(i = 0; i < 2; i++)
 		{
@@ -208,93 +314,103 @@ void read_vcf_data(CHAR *input_filename,pars_t* pars)
         freeze=0;
         num_of_semicolon_before_AF=0; 
 		c = (char) getc(input);
+        char* fbuffer = calloc(10,sizeof(char));
 		while(c != '\t')
 		{
 			c_prev = c;
 			c = (char) getc(input);
 			if(c_prev == ';' && !freeze) num_of_semicolon_before_AF++;
-			if(c_prev == 'A' && c == 'F') freeze = 1;
+			if(c_prev == ';' && c == 'A' && ((c = (char) getc(input))=='F') && ((c = (char) getc(input))=='=')) 
+                freeze = 1;
+                    
+            i = 0;
             if(freeze==1)
             {
-                c = (char) getc(input);
-                AF[block->candidate_num]=(char) getc(input);
+                
+                while((c = (char) getc(input)) != ';' && (c != '\t') && (c!= '\n'))
+                {
+                    fbuffer[i] = c;
+                 //   printf("%c",c);
+                    i++;
+                }    
+               // printf("\n");
+                af = atof(fbuffer);
+                block->AF[block->candidate_num]= af;
+                freeze=0; 
+                
             }
 		}
+        free(fbuffer);
 
    	///////////////////////////////////////////////
-
+        // read field with GT:
+        freeze=0;
         num_of_colon_before_GT = 0;
 		c = (char) getc(input);
         while(c != '\t')
 		{
 			c_prev = c;
 			c = (char) getc(input);
-			if(c_prev == ':') num_of_colon_before_GT++;
-			if(c_prev == 'G' && c == 'T') freeze = 1;
+			if(c_prev == ':'&& freeze==0) num_of_colon_before_GT++;
+			if(c_prev == 'G' && c == 'T') 
+            {
+                freeze = 1;
+            }
+            
 		}
 
+        // read field after GT
 		for(i = 0; i < pars->sample_num; i++)
 		{
-			for(j = 0; j < num_of_colon_before_GT; j++)
+            for(j = 0; j < num_of_colon_before_GT; j++)
 			{
 				while(c != ':') c = (char) getc(input);
 				c = (char) getc(input);
 			}
 
-
-
-            block->_GT[block->candidate_num][i]=0;
+            c = (char) getc(input);
             while(c != ':' && c != '\t' && c != '\n')
             {
                 c_prev = c;
                 c = (char) getc(input);
-                
-                block->_GT[block->candidate_num][i] = block->GT[block->candidate_num][i] * 10 + c - '0';
+                c_next = (char) getc(input);     
+                block->GT[block->candidate_num][i] = (c_prev-'0')+(c_next-'0');
                 c = (char) getc(input);
             }
-            c = (char) getc(input);
+            while((c = (char) getc(input)) != '\t') 
+            {
+                if (c == EOF)
+                {
+                    printf("End of file\n");
+                    break;
+                }
+            }
 
 			///////////////////////////////////////////////
 
 		}
         
-        add_candidate_into_gl_block(block_t* block, pars_t* pars, int64_t locus,int** saved_GT)
-
-
-
-
-
-
-
-
+        add_candidate_into_gl_block(block,pars,locus);
 
      }
+     
+     find_KNN(block,pars,indel_knn);
+
+
+
+
 }
 
 
-int main(int argc char *argv[])
+int main(int argc, char* argv[])
 {
     pars_t* pars =  (pars_t*) calloc(1,sizeof(pars_t));
    // set_default_pars(pars);
     read_vcf_data(argv[1], pars);
+    printf("DONE!!!/n");
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-}
-:q
-
-:q
+} 
